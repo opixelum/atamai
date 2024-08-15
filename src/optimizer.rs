9173996cd3d26f3f2f59ced40a71b96dd1aed30d
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-08-15 23:26:08 +0200

    Add RMS prop

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,5 +1,7 @@
 use ndarray::Array2;
 
+const EPSILON: f64 = f64::MIN_POSITIVE;
+
 pub enum Optimizer {
     Adam(f64, f64, f64), // Learning rate, beta1 & beta2
     Momentum(f64, f64),  // Learning rate & momentum factor
@@ -7,18 +9,36 @@ pub enum Optimizer {
     SGD(f64),            // Learning rate
 }
 
-pub fn update_weights(optimizer: Optimizer, weights: &mut Array2<f64>, gradients: &Array2<f64>) {
-    match optimizer {
-        Optimizer::Adam(learning_rate, beta1, beta2) => {
-            adam(weights, gradients, learning_rate, beta1, beta2)
+struct Optimizer2D {
+    optimizer: Optimizer,
+    velocity: f64,
+}
+
+impl Optimizer2D {
+    pub fn new(optimizer: Optimizer) -> Self {
+        Optimizer2D {
+            optimizer,
+            velocity: 0.,
         }
-        Optimizer::Momentum(learning_rate, momentum_factor) => {
-            momentum(weights, gradients, learning_rate, momentum_factor)
+    }
+
+    pub fn update_weights(&mut self, weights: &mut Array2<f64>, gradients: &Array2<f64>) {
+        match self.optimizer {
+            Optimizer::Adam(learning_rate, beta1, beta2) => {
+                adam(weights, gradients, learning_rate, beta1, beta2)
+            }
+            Optimizer::Momentum(learning_rate, momentum_factor) => {
+                momentum(weights, gradients, learning_rate, momentum_factor)
+            }
+            Optimizer::RMSProp(learning_rate, decay_rate) => rms_prop(
+                weights,
+                gradients,
+                learning_rate,
+                decay_rate,
+                &mut self.velocity,
+            ),
+            Optimizer::SGD(learning_rate) => sgd(weights, gradients, learning_rate),
         }
-        Optimizer::RMSProp(learning_rate, decay_rate) => {
-            rms_prop(weights, gradients, learning_rate, decay_rate)
-        }
-        Optimizer::SGD(learning_rate) => sgd(weights, gradients, learning_rate),
     }
 }
 
@@ -39,7 +59,23 @@ fn momentum(
 ) {
 }
 
-fn rms_prop(weights: &mut Array2<f64>, gradients: &Array2<f64>, learning_rate: f64, decay: f64) {}
+fn rms_prop(
+    weights: &mut Array2<f64>,
+    gradients: &Array2<f64>,
+    learning_rate: f64,
+    decay_rate: f64,
+    velocity: &mut f64,
+) {
+    for neuron_idx in 0..weights.shape()[0] {
+        for weight_idx in 0..weights.shape()[1] {
+            *velocity = *velocity * decay_rate
+                + (1. - decay_rate) * gradients[(neuron_idx, weight_idx)].powi(2);
+
+            weights[(neuron_idx, weight_idx)] = weights[(neuron_idx, weight_idx)]
+                - learning_rate * gradients[(neuron_idx, weight_idx)] / (velocity.sqrt() + EPSILON)
+        }
+    }
+}
 
 fn sgd(weights: &mut Array2<f64>, gradients: &Array2<f64>, learning_rate: f64) {
     for neuron_idx in 0..weights.shape()[0] {
