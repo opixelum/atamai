da12a81be436093f285502d18b8635ac1c733414
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-08-15 12:26:06 -0400

    Add losses (#13)
    * Ignore idea directory
    
    * Add dot in readme
    
    * Add BCE
    
    * Add test for MAE
    
    * Add MSE
    
    * Fix BCE
    
    * Remove CCE for now

---
+++ src/loss.rs
@@ -0,0 +1,42 @@
+use ndarray::Array2;
+
+const EPSILON: f64 = f64::MIN_POSITIVE;
+
+pub enum Loss {
+    BinaryCrossEntropy,
+    MeanAbsoluteError,
+    MeanSquaredError,
+}
+
+pub fn loss(loss: Loss, targets: Array2<f64>, predictions: Array2<f64>) -> f64 {
+    match loss {
+        Loss::BinaryCrossEntropy => binary_cross_entropy(targets, predictions),
+        Loss::MeanAbsoluteError => mean_absolute_error(targets, predictions),
+        Loss::MeanSquaredError => mean_squared_error(targets, predictions),
+    }
+}
+
+fn binary_cross_entropy(targets: Array2<f64>, predictions: Array2<f64>) -> f64 {
+    let mut sum: f64 = 0.;
+    for (i, prediction) in predictions.iter().enumerate() {
+        sum += targets[(i, 0)] * (prediction + EPSILON).ln()
+            + (1. - targets[(i, 0)]) * (1. - prediction + EPSILON).ln();
+    }
+    -sum / predictions.len() as f64
+}
+
+fn mean_absolute_error(targets: Array2<f64>, predictions: Array2<f64>) -> f64 {
+    let mut sum: f64 = 0.;
+    for (i, prediction) in predictions.iter().enumerate() {
+        sum += (prediction - targets[(i, 0)]).abs();
+    }
+    sum / predictions.len() as f64
+}
+
+fn mean_squared_error(targets: Array2<f64>, predictions: Array2<f64>) -> f64 {
+    let mut sum: f64 = 0.;
+    for (i, prediction) in predictions.iter().enumerate() {
+        sum += (prediction - targets[(i, 0)]).powi(2);
+    }
+    sum / predictions.len() as f64
+}
