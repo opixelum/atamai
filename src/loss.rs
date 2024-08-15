f1e2e8dbccb2cf2f2697891d226527c76bccff8a
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-08-15 17:47:23 +0200

    Fix BCE

--- src/loss.rs
+++ src/loss.rs
@@ -5,45 +5,37 @@ const EPSILON: f64 = f64::MIN_POSITIVE;
 pub enum Loss {
     BinaryCrossEntropy,
     CategoricalCrossEntropy,
-    HingeLoss,
-    HuberLoss,
     MeanAbsoluteError,
     MeanSquaredError,
 }
 
-pub fn loss(loss: Loss, predictions: Array1<f64>, targets: Array1<f64>) -> f64 {
+pub fn loss(loss: Loss, targets: Array1<f64>, predictions: Array1<f64>) -> f64 {
     match loss {
-        Loss::BinaryCrossEntropy => binary_cross_entropy(predictions, targets),
-        Loss::CategoricalCrossEntropy => categorical_cross_entropy(predictions, targets),
-        Loss::HingeLoss => 0.,
-        Loss::HuberLoss => 0.,
-        Loss::MeanAbsoluteError => mean_absolute_error(predictions, targets),
-        Loss::MeanSquaredError => mean_squared_error(predictions, targets),
+        Loss::BinaryCrossEntropy => binary_cross_entropy(targets, predictions),
+        Loss::CategoricalCrossEntropy => categorical_cross_entropy(targets, predictions),
+        Loss::MeanAbsoluteError => mean_absolute_error(targets, predictions),
+        Loss::MeanSquaredError => mean_squared_error(targets, predictions),
     }
 }
 
-fn binary_cross_entropy(predictions: Array1<f64>, targets: Array1<f64>) -> f64 {
+fn binary_cross_entropy(targets: Array1<f64>, predictions: Array1<f64>) -> f64 {
     let mut sum: f64 = 0.;
     for (i, prediction) in predictions.iter().enumerate() {
-        sum += targets[i] * (prediction + EPSILON).log10()
-            + (1. * targets[i]) * (1. - prediction + EPSILON).log10()
+        sum += targets[i] * (prediction + EPSILON).ln()
+            + (1. - targets[i]) * (1. - prediction + EPSILON).ln();
     }
     -sum / predictions.len() as f64
 }
 
-fn categorical_cross_entropy(predictions: Array1<f64>, targets: Array1<f64>) -> f64 {
+fn categorical_cross_entropy(targets: Array1<f64>, predictions: Array1<f64>) -> f64 {
     let mut sum: f64 = 0.;
     for (i, prediction) in predictions.iter().enumerate() {
-        sum += targets[i] * (prediction + EPSILON).log10()
+        sum += targets[i] * (prediction + EPSILON).ln()
     }
     sum
 }
 
-// fn hinge_loss(predictions: Array1<f64>, targets: Array1<f64>) -> f64 {}
-
-// fn huber_loss(predictions: Array1<f64>, targets: Array1<f64>) -> f64 {}
-
-fn mean_absolute_error(predictions: Array1<f64>, targets: Array1<f64>) -> f64 {
+fn mean_absolute_error(targets: Array1<f64>, predictions: Array1<f64>) -> f64 {
     let mut sum: f64 = 0.;
     for (i, prediction) in predictions.iter().enumerate() {
         sum += (prediction - targets[i]).abs();
@@ -51,7 +43,7 @@ fn mean_absolute_error(predictions: Array1<f64>, targets: Array1<f64>) -> f64 {
     sum / predictions.len() as f64
 }
 
-fn mean_squared_error(predictions: Array1<f64>, targets: Array1<f64>) -> f64 {
+fn mean_squared_error(targets: Array1<f64>, predictions: Array1<f64>) -> f64 {
     let mut sum: f64 = 0.;
     for (i, prediction) in predictions.iter().enumerate() {
         sum += (prediction - targets[i]).powi(2);
--- tests/loss.rs
+++ tests/loss.rs
@@ -1,50 +1,30 @@
-use ndarray::{array, Array1};
-
 use atamai::loss;
 use atamai::loss::Loss::*;
+use ndarray::array;
 
 #[test]
 fn binary_cross_entropy() {
-    let mut predictions: Array1<f64> = array![0.];
-    let mut targets: Array1<f64> = array![0.];
-    let mut loss: f64 = loss::loss(BinaryCrossEntropy, predictions, targets);
-    let mut expected_loss: f64 = 0.;
-    assert_eq!(loss, expected_loss);
-
-    // TODO: check with TensorFlow
-    predictions = array![0., 1., 0., 0.];
-    targets = array![0.6, 0.3, 0.2, 0.8];
-    loss = loss::loss(BinaryCrossEntropy, predictions, targets);
-    expected_loss = 0.865; // to update
+    let targets = array![0., 1., 0., 0.];
+    let predictions = array![0.6, 0.3, 0.2, 0.8];
+    let loss = loss::loss(BinaryCrossEntropy, targets, predictions);
+    let expected_loss = 0.9882112499871003;
     assert_eq!(loss, expected_loss);
 }
 
 #[test]
 fn mean_absolute_error() {
-    let mut predictions: Array1<f64> = array![0.];
-    let mut targets: Array1<f64> = array![0.];
-    let mut loss: f64 = loss::loss(MeanAbsoluteError, predictions, targets);
-    let mut expected_loss: f64 = 0.;
-    assert_eq!(loss, expected_loss);
-
-    predictions = array![12., 15., 10., 20., 18.];
-    targets = array![10., 15., 12., 18., 20.];
-    loss = loss::loss(MeanAbsoluteError, predictions, targets);
-    expected_loss = 1.6;
+    let targets = array![10., 15., 12., 18., 20.];
+    let predictions = array![12., 15., 10., 20., 18.];
+    let loss = loss::loss(MeanAbsoluteError, targets, predictions);
+    let expected_loss = 1.6;
     assert_eq!(loss, expected_loss);
 }
 
 #[test]
 fn mean_squared_error() {
-    let mut predictions: Array1<f64> = array![0.];
-    let mut targets: Array1<f64> = array![0.];
-    let mut loss: f64 = loss::loss(MeanSquaredError, predictions, targets);
-    let mut expected_loss: f64 = 0.;
-    assert_eq!(loss, expected_loss);
-
-    predictions = array![12., 18., 32., 38., 48.];
-    targets = array![10., 20., 30., 40., 50.];
-    loss = loss::loss(MeanSquaredError, predictions, targets);
-    expected_loss = 4.;
+    let targets = array![10., 20., 30., 40., 50.];
+    let predictions = array![12., 18., 32., 38., 48.];
+    let loss = loss::loss(MeanSquaredError, targets, predictions);
+    let expected_loss = 4.;
     assert_eq!(loss, expected_loss);
 }
